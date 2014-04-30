a37f76c01144b778e2f27ed0d2cc456eb659bdd5
pgarber <pgarber108@gmail.com>
2014-04-30 09:20:38 -0400

    refining idea views

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -4,4 +4,18 @@ module IdeasHelper
     idea = Idea.find_by(id: params[:id])  # return idea is implied
   end
 
+  def num_votes(idea)
+  	puts "in num_votes, idea.body: #{idea.body}"
+  	@idea_upvotes=0
+  	@idea_downvotes=0
+  	idea.votes.each do | vote |
+  	  if vote == 1
+  	  	puts "upvote"
+  	    @idea_upvotes += 1
+  	  elsif	vote == -1
+  	  	puts "downvote"
+  	  	@idea_downvotes -- 1
+  	  end
+  	end
+  end
 end
