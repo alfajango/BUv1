1a82bc96b45714f16c72ac2c4e7a6beeb5bc7400
pgarber <pgarber108@gmail.com>
2014-05-02 17:01:36 -0400

    votes record correctly

---
+++ app/assets/javascripts/ideas.js
@@ -0,0 +1,10 @@
+//# Place all the behaviors and hooks related to the matching controller here.
+//# All this logic will automatically be available in application.js.
+//# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+// from http://stackoverflow.com/questions/15247184/angularjs-equivalent-to-jquery-toggle-show-hide-a-section
+
+'use strict';
+var App=angular.module('myApp',[]);
+function Ctrl($scope){
+}
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,20 +1,22 @@
 class VotesController < ApplicationController
 
   def create
-    #Vote.create(upvote: upvote, downvote: downvote, idea: idea, created: Time.now,  user: current_user)
+
     puts "params: #{params}"
     puts "params[:vote]: #{params[:vote]}"
-    puts "params[:vote][:upvote]: #{params[:vote][:upvote]}"
+    puts "params[:vote][:idea_id]: #{params[:vote][:idea_id]}"
 
-    #if params[:vote][:up] == 1
+    idea = Idea.find_by(id: params[:vote][:idea_id])
+    puts "votes_controller idea.body: #{idea.body}"
     @vote = Vote.new(params[:vote])
-    
-    # if @vote.save
-    #   flash[:success] = "Thanks for the vote"
-    #   redirect_to '#'
-    # else
-    #   flash[:success] = "FAIL"
-    # end
+    idea.votes << @vote
+    current_user.votes << @vote
+    if @vote.save
+      flash[:success] = "Thanks for the vote"
+      redirect_to '#'
+    else
+      flash[:success] = "FAIL"
+    end
   end
 
   def update
@@ -22,25 +24,4 @@ class VotesController < ApplicationController
   end
 
 
-  # private
-
-  #   def vote_params
-  #     #puts "params[:project][:company_id]: #{params[:project][:company_id]}"  # correct
-  #     #puts "params[:project][:name]: #{params[:project][:name]}"  # correct
-  #     vparam = {}   # define it as a hash so it doesn't think it's an array!
-  #     #vparam[:]
-  #     vparam[:user] = params[:vote][:user]
-  #     puts "vparam: #{vparam}"
-  #     vparam[:idea] = params[:vote][:idea]
-  #     puts "vparam: #{vparam}"
-  #     vparam[:downvote] = params[:vote][:downvote]
-  #     puts "vparam: #{vparam}"
-  #     vparam[:upvote] = params[:vote][:upvote]
-  #     puts "vparam: #{vparam}"
-  #     vparam[:created] = params[:vote][:created]
-  #     puts "vparam: #{vparam}"
-  #     #puts "2nd pparam: #{pparam}"
-  #     return vparam
-
-  #   end
 end
