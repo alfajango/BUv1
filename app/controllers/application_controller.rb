c3356c50ae690984fc732b304727c755b80dfe0b
pgarber <pgarber108@gmail.com>
2014-05-29 14:16:19 -0400

    create colleague from sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # the code below allows me to render a new colleage form in the sidebar
+  before_filter :load_newuser
+
+  def load_newuser
+  	@newuser = User.new
+
+  end
 end
