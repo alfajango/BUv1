5e60c67583f96e6319ec63f93197a39243123e08
pgarber <pgarber108@gmail.com>
2014-05-29 15:47:30 -0400

    fix create pwd page

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "phildefault@example.com"
+  config.mailer_sender = "contact@prietary.com.com"
 
   # Configure the class responsible to send e-mails.
   config.mailer = "Devise::Mailer" # PG enable 4/25/14
